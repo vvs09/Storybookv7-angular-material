74d454fab6454f29e8c37826b906a42016a66660
vvs09 <137658234+vvs09@users.noreply.github.com>
2023-12-25 18:12:06 -0500

    Stortbook ligth and dark themes

--- storybook/.storybook/preview.ts
+++ storybook/.storybook/preview.ts
@@ -1,6 +1,7 @@
 import { importProvidersFrom } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Preview, applicationConfig } from '@storybook/angular';
+import { themes } from '@storybook/theming';
 
 const preview: Preview = {
   parameters: {
@@ -11,6 +12,11 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
+    darkMode: {
+      dark: { ...themes.dark },
+      light: { ...themes.light},
+      current:'light'
+    }
   },
   decorators: [
     applicationConfig({
