b60f0effd41f1e155ff188dc45ec07c0363648b5
vvs09 <137658234+vvs09@users.noreply.github.com>
2024-01-27 12:37:08 -0500

    Button refactor

--- storybook/src/stories/button.stories.ts
+++ storybook/src/stories/button.stories.ts
@@ -5,6 +5,7 @@ import { MatButtonModule, MatButton } from '@angular/material/button';
 const meta: Meta<MatButton & { label: string; buttonType: string }> = {
   title: 'Components/Button',
   component: MatButton,
+  tags: ['autodocs'],
   argTypes: {
     color: {
       control: 'radio',
@@ -12,7 +13,10 @@ const meta: Meta<MatButton & { label: string; buttonType: string }> = {
       description: 'Theme color palette for the component.',
       table: {
         category: 'INPUTS',
-        defaultValue: { summary: '-' },
+        defaultValue: { summary: 'null' },
+        type: {
+          summary: 'string',
+        },
       },
     },
     disabled: {
@@ -21,7 +25,10 @@ const meta: Meta<MatButton & { label: string; buttonType: string }> = {
         'Whether the checkbox is disabled. This fully overrides the implementation provided by mixinDisabled, but the mixin is still required because mixinTabIndex requires it.',
       table: {
         category: 'INPUTS',
-        defaultValue: { summary: '-' },
+        defaultValue: { summary: 'false' },
+        type: {
+          summary: 'boolean',
+        },
       },
     },
     disableRipple: {
@@ -29,23 +36,59 @@ const meta: Meta<MatButton & { label: string; buttonType: string }> = {
       description: 'Whether ripples are disabled.',
       table: {
         category: 'INPUTS',
-        defaultValue: { summary: '-' },
+        defaultValue: { summary: 'false' },
+        type: {
+          summary: 'boolean',
+        },
       },
     },
-    // defaultColor: {
-    //   control: 'text',
-    //   description: 'Default color to fall back to if no value is set.',
-    //   table: {
-    //     category: 'INPUTS',
-    //     defaultValue: { summary: '-' },
-    //   },
-    // },
-    ripple: {
-      control: 'object',
-      description: 'Reference to the MatRipple instance of the button.',
+    buttonType: {
+      control: 'select',
+      options: [
+        'mat-button',
+        'mat-raised-button',
+        'mat-stroked-button',
+        'mat-flat-button',
+      ],
+      description: 'Type of buton.',
+      table: {
+        defaultValue: { summary: 'mat-button' },
+        type: {
+          summary: 'string',
+        },
+      },
+    },
+    label: {
+      control: 'text',
+      description: 'Button label.',
+      table: {
+        defaultValue: { summary: 'button' },
+        type: {
+          summary: 'string',
+        },
+      },
+    },
+    ariaDisabled: {
+      control: 'boolean',
+      description: 'aria-disabled value of the button.',
       table: {
         category: 'INPUTS',
-        defaultValue: { summary: '-' },
+        defaultValue: { summary: 'undefined' },
+        type: {
+          summary: 'boolean | undefined',
+        },
+      },
+    },
+    disabledInteractive: {
+      control: 'boolean',
+      description:
+        'Natively disabled buttons prevent focus and any pointer events from reaching the button. In some scenarios this might not be desirable, because it can prevent users from finding out why the button is disabled (e.g. via tooltip). Enabling this input will change the button so that it is styled to be disabled and will be marked as aria-disabled, but it will allow the button to receive events and focus. Note that by enabling this, you need to set the tabindex yourself if the button isnt meant to be tabbable and you have to prevent the button action (e.g. form submissions).',
+      table: {
+        category: 'INPUTS',
+        defaultValue: { summary: 'false' },
+        type: {
+          summary: 'boolean',
+        },
       },
     },
   },
@@ -57,10 +100,18 @@ const meta: Meta<MatButton & { label: string; buttonType: string }> = {
   render: (args: MatButton & { label: string; buttonType: string }) => ({
     props: args,
     template: `
-    <button  *ngIf="buttonType === 'mat-button'" mat-button  ${argsToTemplate(args)} >{{label}}</button>
-    <button *ngIf="buttonType === 'mat-raised-button' " mat-raised-button ${argsToTemplate(args)} >{{label}}</button>
-    <button *ngIf="buttonType === 'mat-stroked-button' " mat-stroked-button ${argsToTemplate(args)} >{{label}}</button>
-    <button *ngIf="buttonType === 'mat-flat-button' "  mat-flat-button ${argsToTemplate(args)} >{{label}}</button>
+    <button  *ngIf="buttonType === 'mat-button'" mat-button  ${argsToTemplate(
+      args
+    )} >{{label}}</button>
+    <button *ngIf="buttonType === 'mat-raised-button' " mat-raised-button ${argsToTemplate(
+      args
+    )} >{{label}}</button>
+    <button *ngIf="buttonType === 'mat-stroked-button' " mat-stroked-button ${argsToTemplate(
+      args
+    )} >{{label}}</button>
+    <button *ngIf="buttonType === 'mat-flat-button' "  mat-flat-button ${argsToTemplate(
+      args
+    )} >{{label}}</button>
 \    `,
   }),
 };
@@ -71,9 +122,10 @@ type Story = StoryObj<MatButton & { label: string; buttonType: string }>;
 export const accent: Story = {
   args: {
     color: 'accent',
-    // defaultColor: 'accent',
     disabled: false,
-    disableRipple: false,
+    ariaDisabled: false,
+    disabledInteractive: false,
+    disableRipple:false,
     label: 'Button',
     buttonType: 'mat-button',
   },
@@ -157,29 +209,29 @@ export const MatStrokedButtonDisabled: Story = {
 };
 
 export const MatFlatButtonAccent: Story = {
-    args: {
-      ...accent.args,
-      buttonType: 'mat-flat-button',
-    },
-  };
-  
-  export const  MatFlatButtonPrimary: Story = {
-    args: {
-      ...MatFlatButtonAccent.args,
-      color: 'primary',
-    },
-  };
-  
-  export const  MatFlatButtonWarn: Story = {
-    args: {
-      ...MatFlatButtonAccent.args,
-      color: 'warn',
-    },
-  };
-  
-  export const  MatFlatButtonDisabled: Story = {
-    args: {
-      ...MatFlatButtonAccent.args,
-      disabled: true,
-    },
-  };
+  args: {
+    ...accent.args,
+    buttonType: 'mat-flat-button',
+  },
+};
+
+export const MatFlatButtonPrimary: Story = {
+  args: {
+    ...MatFlatButtonAccent.args,
+    color: 'primary',
+  },
+};
+
+export const MatFlatButtonWarn: Story = {
+  args: {
+    ...MatFlatButtonAccent.args,
+    color: 'warn',
+  },
+};
+
+export const MatFlatButtonDisabled: Story = {
+  args: {
+    ...MatFlatButtonAccent.args,
+    disabled: true,
+  },
+};
