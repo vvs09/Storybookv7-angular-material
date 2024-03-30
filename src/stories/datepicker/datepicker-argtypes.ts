b7dcad494852ceb0f1af951e0961717cccab11fb
vvs09 <137658234+vvs09@users.noreply.github.com>
2024-03-29 22:27:48 -0400

    datepicker

---
+++ src/stories/datepicker/datepicker-argtypes.ts
@@ -0,0 +1,13 @@
+export const datepickerArgTypes = {
+  disabled: {
+    control: 'boolean',
+    description: 'Whether the badge is disabled.',
+    table: {
+      category: 'INPUTS',
+      defaultValue: { summary: 'false' },
+      type: {
+        summary: 'boolean',
+      },
+    },
+  },
+};
